2cf62ba646235dfdbb3f50ed5a5b03de5df895e1
Fei-7 <supachokang@gmail.com>
2024-09-15 16:24:59 +0700

    chore: add json tag to model.Club

--- model/club.go
+++ model/club.go
@@ -1,7 +1,7 @@
 package model
 
 type Club struct {
-	ID          string `bson:"_id"`
-	Name        string `bson:"name"`
-	Description string `bson:"description"`
+	ID          string `bson:"_id" json:"id"`
+	Name        string `bson:"name" json:"name"`
+	Description string `bson:"description" json:"description"`
 }
